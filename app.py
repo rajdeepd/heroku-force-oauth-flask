0d999051264633808d187c9f9d221472e5763d48
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-11-04 15:03:35 +0530

    added test certificiate, private key and app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
 import urllib
+from OpenSSL import SSL
+import os
 import urllib2
+#from urllib2 import urlopen
 app = Flask(__name__)
 
-client_id = ''
-client_secret = ''
+client_id = '3MVG9ZL0ppGP5UrBMsor2EoRrW0__Bqk8ytOi7_7T.afqqthzPfztjH8MLeT8bfkYwz45_3mVUJO5pdWE_EJa'
+client_secret = '5045264606824676005'
 url = 'https://login.salesforce.com/services/oauth2/authorize?response_type=code&' \
-    'client_id=' + client_id + '&redirect_uri=http://localhost:5000/auth/salesforce/callback&prompt=login%20consent'
+    'client_id=' + client_id + '&redirect_uri=https://127.0.0.1:5000/auth/salesforce/callback&prompt=login%20consent'
+context = SSL.Context(SSL.SSLv23_METHOD)
+cer = os.path.join(os.path.dirname(__file__), 'test.crt')
+key = os.path.join(os.path.dirname(__file__), 'test.key')
 
 @app.route('/')
 @app.route('/login', methods=['GET'])
@@ -23,7 +29,7 @@ def callback():
               'grant_type' : 'authorization_code',
               'client_id' : client_id,
               'client_secret' : client_secret,
-               'redirect_uri' : 'http://localhost:5000/auth/salesforce/callback'}
+               'redirect_uri' : 'https://127.0.0.1:5000/auth/salesforce/callback'}
 
     data = urllib.urlencode(values)
     req = urllib2.Request(url_post, data)
@@ -33,4 +39,5 @@ def callback():
 
 
 if __name__ == '__main__':
-    app.run()
+    context = (cer, key)
+    app.run( host='0.0.0.0', port=5000, debug = True, ssl_context=context)
